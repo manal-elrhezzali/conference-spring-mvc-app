6d6a887474200c8cb03b2e707802070725d5142c
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-04-24 19:08:20 +0200

    Passing parameters + @ModelAttribute + @PostMapping

--- src/main/java/com/manal/conferencespringmvc/controller/RegistrationController.java
+++ src/main/java/com/manal/conferencespringmvc/controller/RegistrationController.java
@@ -1,14 +1,25 @@
 package com.manal.conferencespringmvc.controller;
 
+import com.manal.conferencespringmvc.model.Registration;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
 
   @GetMapping("registration")
-  public String getRegistration(Map<String, Object> model) {
+  //and now we have our object bound by our model to this ModelAttribute
+  public String getRegistration(@ModelAttribute ("registration")Registration registration) {
+    return "registration";
+  }
+
+  @PostMapping("registration")
+  //and now we have our object bound by our model to this ModelAttribute
+  public String addRegistration(@ModelAttribute ("registration")Registration registration) {
+    System.out.println("Registration : " + registration.getName());
     return "registration";
   }
 }
